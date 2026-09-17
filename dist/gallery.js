79a91adc53f0dbdf19d40d7d6abf5bf0dafe6cb2
sui <96000378+otctoken@users.noreply.github.com>
2026-09-17 17:14:41 +0800

    Update gallery.js

--- dist/gallery.js
+++ dist/gallery.js
@@ -12,4 +12,6 @@ window.STFU_IMAGES = [
   { src: 'https://ik.imagekit.io/4xf5jb5grv/sticke43212r.png', title: 'There is no gap.', alt: 'There is no gap.' },
   { src: 'https://ik.imagekit.io/4xf5jb5grv/st13331icker.webp', title: 'LET THEM COOK', alt: 'Food photography' },
   { src: 'https://ik.imagekit.io/4xf5jb5grv/stic133333ker.webp', title: 'STFU', alt: 'STFU' },
+   { src: 'https://ik.imagekit.io/4xf5jb5grv/%E5%B1%8F%E5%B9%95%E6%88%AA%E5%9B%BE%202026-09-17%20171037.png', title: 'price', alt: 'price' },
+   { src: 'https://ik.imagekit.io/4xf5jb5grv/%E5%B1%8F%E5%B9%95%E6%88%AA%E5%9B%BE%202026-09-17%20171211.png', title: 'TVL', alt: 'TVL' },
 ];
