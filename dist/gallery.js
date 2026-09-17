bc41064e3585d3cafd96caf84ea84a31e8105705
sui <96000378+otctoken@users.noreply.github.com>
2026-09-17 11:02:15 +0800

    Update gallery.js

--- dist/gallery.js
+++ dist/gallery.js
@@ -9,7 +9,7 @@ window.STFU_IMAGES = [
   { src: 'https://ik.imagekit.io/4xf5jb5grv/%E5%B1%8F%E5%B9%95%E6%88%AA%E5%9B%BE%202026-09-17%20095619.png', title: 'Excellent API', alt: 'Excellent API' },
   { src: 'https://ik.imagekit.io/4xf5jb5grv/%E5%B1%8F%E5%B9%95%E6%88%AA%E5%9B%BE%202026-09-17%20094416.png', title: 'Very safe', alt: 'Very safe' },
   { src: 'https://ik.imagekit.io/4xf5jb5grv/%E5%B1%8F%E5%B9%95%E6%88%AA%E5%9B%BE%202026-09-17%20095228.png', title: 'It s very fast.', alt: 'fast' },
-  { src: 'https://ik.imagekit.io/4xf5jb5grv/sticke43212r.webp', title: 'There is no gap.', alt: 'There is no gap.' },
+  { src: 'https://ik.imagekit.io/4xf5jb5grv/sticke43212r.png', title: 'There is no gap.', alt: 'There is no gap.' },
   { src: 'https://ik.imagekit.io/4xf5jb5grv/st13331icker.webp', title: 'LET THEM COOK', alt: 'Food photography' },
   { src: 'https://ik.imagekit.io/4xf5jb5grv/stic133333ker.webp', title: 'STFU', alt: 'STFU' },
 ];
